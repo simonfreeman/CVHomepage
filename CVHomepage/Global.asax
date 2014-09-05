5528d8d8b9eba6fc767b8938deb34c7984b2d693
simonfreeman <spfreeman@live.co.uk>
2014-09-05 06:39:11 +0100

    Initial

---
+++ CVHomepage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CVHomepage.MvcApplication" Language="C#" %>
---
+++ CVHomepage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CVHomepage.Startup))]
+namespace CVHomepage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
