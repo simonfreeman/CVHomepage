5d2f8981ddf28ce616498e20ace3d991819373f8
simonfreeman <spfreeman@live.co.uk>
2014-09-06 15:17:55 +0100

    removed/changed some default generated texts

--- CVHomepage/Controllers/HomeController.cs
+++ CVHomepage/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace CVHomepage.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About Me.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Me.";
 
             return View();
         }
