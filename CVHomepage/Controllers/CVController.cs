23485967f54741454406bc2be0c26d87cb52c8c5
simonfreeman <spfreeman@live.co.uk>
2014-09-10 16:45:11 +0100

    Generated CV controller, changed views so they don't display the user

---
+++ CVHomepage/Controllers/CVController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CVHomepage.Models;
+using CVHomepage.DAL;
+
+namespace CVHomepage.Controllers
+{
+    public class CVController : Controller
+    {
+        private CVContext db = new CVContext();
+
+        // GET: /CV/
+        public ActionResult Index()
+        {
+            return View(db.CVs.ToList());
+        }
+
+        // GET: /CV/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CV cv = db.CVs.Find(id);
+            if (cv == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cv);
+        }
+
+        // GET: /CV/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /CV/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Name,User")] CV cv)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CVs.Add(cv);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cv);
+        }
+
+        // GET: /CV/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CV cv = db.CVs.Find(id);
+            if (cv == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cv);
+        }
+
+        // POST: /CV/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Name,User")] CV cv)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cv).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cv);
+        }
+
+        // GET: /CV/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CV cv = db.CVs.Find(id);
+            if (cv == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cv);
+        }
+
+        // POST: /CV/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CV cv = db.CVs.Find(id);
+            db.CVs.Remove(cv);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
