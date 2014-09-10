4402b7e628dd18703550305cb4cd39c83ebe9086
simonfreeman <spfreeman@live.co.uk>
2014-09-10 13:53:37 +0100

    Changed colour scheme, added active menu items, credit to Chris Way http://chrisondotnet.com/2012/08/setting-active-link-twitter-bootstrap-navbar-aspnet-mvc/ for the helper

---
+++ CVHomepage/Helpers/HTMLHelpers.cs
@@ -0,0 +1,26 @@
+﻿//credit to Chris Way for this at http://chrisondotnet.com/2012/08/setting-active-link-twitter-bootstrap-navbar-aspnet-mvc/
+
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace CVHomepage.Helpers.HTMLHelpers
+{
+    public static class HtmlHelpers
+    {
+        public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName)
+        {
+            var currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
+            var currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
+
+            var builder = new TagBuilder("li")
+            {
+                InnerHtml = htmlHelper.ActionLink(linkText, actionName, controllerName).ToHtmlString()
+            };
+
+            if (controllerName == currentController && actionName == currentAction)
+                builder.AddCssClass("active");
+
+            return new MvcHtmlString(builder.ToString());
+        }
+    }
+}
