31e0590fc43c67e6499a0ab55a4a4d721bba6b17
simonfreeman <spfreeman@live.co.uk>
2014-09-11 20:26:01 +0100

    Side Menu added for CV builder, HTMLhelpers changed so now menu items can supply additional controller names which they will light up for. Also got rid of the action dependency. I've mostly lost track of time and space.

--- CVHomepage/Helpers/HTMLHelpers.cs
+++ CVHomepage/Helpers/HTMLHelpers.cs
@@ -1,7 +1,8 @@
-﻿//credit to Chris Way for this at http://chrisondotnet.com/2012/08/setting-active-link-twitter-bootstrap-navbar-aspnet-mvc/
+﻿//credit to Chris Way for the base of this at http://chrisondotnet.com/2012/08/setting-active-link-twitter-bootstrap-navbar-aspnet-mvc/
 
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using System.Collections.Generic;
 
 namespace CVHomepage.Helpers.HTMLHelpers
 {
@@ -18,6 +19,23 @@ namespace CVHomepage.Helpers.HTMLHelpers
             };
 
             if (controllerName == currentController && actionName == currentAction)
+                builder.AddCssClass("active");
+
+            return new MvcHtmlString(builder.ToString());
+        }
+
+        public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, string[] additionalNames)
+        {
+
+            var currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
+            var currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
+            List<string> additionalNameList = new List<string>(additionalNames);
+            var builder = new TagBuilder("li")
+            {
+                InnerHtml = htmlHelper.ActionLink(linkText, actionName, controllerName).ToHtmlString()
+            };
+
+            if (controllerName == currentController || additionalNameList.Contains(currentController))
                 builder.AddCssClass("active");
 
             return new MvcHtmlString(builder.ToString());
