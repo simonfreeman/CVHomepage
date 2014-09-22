789e081e3f98d98286d1c548b7942207345a2821
simonfreeman <spfreeman@live.co.uk>
2014-09-22 22:00:38 +0100

    rudimentary fix to the htmlhelpers that allows menulink items to specify any specific action/controllers they shouldnt light up for. This is used in situations where cv manager should light up for 90% of the actions in the cv controller, but 1 (planned feature) has it's own seperate menu item which therfore means cv manager shouldnt light up, otherwise yo have 2 menu items lighting up. Much simpler than i made it sound.

--- CVHomepage/Helpers/HTMLHelpers.cs
+++ CVHomepage/Helpers/HTMLHelpers.cs
@@ -3,6 +3,7 @@
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CVHomepage.Helpers.HTMLHelpers
 {
@@ -30,14 +31,59 @@ namespace CVHomepage.Helpers.HTMLHelpers
             var currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
             var currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
             List<string> additionalNameList = new List<string>(additionalControllerNames);
+
+
             var builder = new TagBuilder("li")
             {
                 InnerHtml = htmlHelper.ActionLink(linkText, actionName, controllerName).ToHtmlString()
             };
 
             if (controllerName == currentController || additionalNameList.Contains(currentController))
+                
                 builder.AddCssClass("active");
 
+            return new MvcHtmlString(builder.ToString());
+        }
+
+        public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, string[] additionalControllerNames, string[,] ignoreActionControllerNames)
+        {
+
+            var currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
+            var currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
+            List<string> additionalNameList = new List<string>(additionalControllerNames);
+
+            var builder = new TagBuilder("li")
+            {
+                InnerHtml = htmlHelper.ActionLink(linkText, actionName, controllerName).ToHtmlString()
+            };
+
+            if (controllerName == currentController || additionalNameList.Contains(currentController))
+            {
+                //checks the current action and controller against the list of 
+                //actions and controller a menu link should ignore. This is for instances 
+                //like where planned features is in the cv controller, but you wouldnt want to light up 
+                //cv manager on the sidebar.
+                int count = 0;
+                string checkIgnore = "";
+                foreach (string ignore in ignoreActionControllerNames)
+                {
+                    if (count % 2 == 0)
+                    {
+                        checkIgnore = ignore;
+                    }
+                    else
+                    {
+                        checkIgnore += ignore;
+                    }
+
+                    if (checkIgnore == currentAction + currentController)
+                    {
+                        return new MvcHtmlString(builder.ToString());
+                    }
+                    count++;
+                }
+                    builder.AddCssClass("active");
+            }
             return new MvcHtmlString(builder.ToString());
         }
     }
